cf7e31066b314f4f4f0e059ddac5b09df257ce5f
Sahib <sahibmobdev@gmail.com>
2023-04-10 20:07:57 +0400

    adding Entities

---
+++ app/src/main/java/com/example/shopinglist/entities/ShopingListNames.kt
@@ -0,0 +1,22 @@
+package com.example.shopinglist.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.io.Serializable
+
+@Entity(tableName = "shopping_list_names")
+data class ShoppingListNames(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int?,
+    @ColumnInfo(name = "name")
+    val name: String,
+    @ColumnInfo(name = "time")
+    val time: String,
+    @ColumnInfo(name = "allItemCounter")
+    val allItemCounter: Int,
+    @ColumnInfo(name = "checkedItemCounter")
+    val checkedItemCounter: Int,
+    @ColumnInfo(name = "itemsIds")
+    val itemsIds: String
+) : Serializable
