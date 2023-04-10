6c7fa76a0aa50a3c42f9110e5f60cd13e4d2330a
Sahib <sahibmobdev@gmail.com>
2023-04-10 21:09:41 +0400

    adding Entities

---
+++ app/src/main/java/com/example/shopinglist/entities/NoteItem.kt
@@ -0,0 +1,20 @@
+package com.example.shopinglist.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "note_list")
+data class NoteItem(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int?,
+    @ColumnInfo(name = "title")
+    val title: String,
+    @ColumnInfo(name = "content")
+    val content: String,
+    @ColumnInfo(name = "time")
+    val time: String,
+    @ColumnInfo(name = "category")
+    val category: String
+)
