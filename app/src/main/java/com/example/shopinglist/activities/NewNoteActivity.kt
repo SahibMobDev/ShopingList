06a587de48674f6b6503320bcfee64173f7599b5
Sahib <sahibmobdev@gmail.com>
2023-04-17 19:19:13 +0400

    Create animation for ColorPicker

--- app/src/main/java/com/example/shopinglist/activities/NewNoteActivity.kt
+++ app/src/main/java/com/example/shopinglist/activities/NewNoteActivity.kt
@@ -8,6 +8,9 @@ import android.text.Spannable
 import android.text.style.StyleSpan
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.core.text.getSpans
 import com.example.shopinglist.R
 import com.example.shopinglist.databinding.ActivityNewNoteBinding
@@ -54,6 +57,12 @@ class NewNoteActivity : AppCompatActivity() {
             setBoldForSelectedText()
         }  else if (item.itemId == android.R.id.home) {
             finish()
+        } else if (item.itemId == R.id.id_color) {
+            if (binding.colorPicker.isShown) {
+                closeColorPicker()
+            } else {
+                openColorPicker()
+            }
         }
         return super.onOptionsItemSelected(item)
     }
@@ -114,5 +123,30 @@ class NewNoteActivity : AppCompatActivity() {
     private fun actionBarSettings() {
         val ab = supportActionBar
         ab?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    private fun openColorPicker() {
+        binding.colorPicker.visibility = View.VISIBLE
+        val openAnim = AnimationUtils.loadAnimation(this, R.anim.open_color_picker)
+        binding.colorPicker.startAnimation(openAnim)
+    }
+
+    private fun closeColorPicker() {
+        val closeAnim = AnimationUtils.loadAnimation(this, R.anim.close_color_picker)
+        closeAnim.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {
+
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                binding.colorPicker.visibility = View.GONE
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {
+
+            }
+
+        })
+        binding.colorPicker.startAnimation(closeAnim)
     }
 }
