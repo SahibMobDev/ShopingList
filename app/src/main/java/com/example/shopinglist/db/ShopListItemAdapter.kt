d705e94d9081609baeb464b8f10c49b5d667a7cc
Sahib <sahibmobdev@gmail.com>
2023-04-25 02:41:15 +0400

    Create XMLs for ShopListItemAdapter

--- app/src/main/java/com/example/shopinglist/db/ShopListItemAdapter.kt
+++ app/src/main/java/com/example/shopinglist/db/ShopListItemAdapter.kt
@@ -48,11 +48,11 @@ class ShopListItemAdapter(private val listener: Listener) : ListAdapter<Shopping
         companion object {
             fun createShopItem(parent: ViewGroup) : ItemHolder {
                 return ItemHolder(LayoutInflater.from(parent.context)
-                    .inflate(R.layout.list_name_item, parent, false))
+                    .inflate(R.layout.shop_list_item, parent, false))
             }
             fun createLibraryItem(parent: ViewGroup) : ItemHolder {
                 return ItemHolder(LayoutInflater.from(parent.context)
-                    .inflate(R.layout.list_name_item, parent, false))
+                    .inflate(R.layout.shop_library_list_item, parent, false))
             }
         }
     }
